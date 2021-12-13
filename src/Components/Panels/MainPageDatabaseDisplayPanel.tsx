f41c3f2210f205a8042d2cbb1839d14d4192d244
Cameron Thacker <cthacker@udel.edu>
2021-12-13 11:44:04 -0500

    Added table to display database contents

--- src/Components/Panels/MainPageDatabaseDisplayPanel.tsx
+++ src/Components/Panels/MainPageDatabaseDisplayPanel.tsx
@@ -1,10 +1,43 @@
-
+import React from "react";
+import { Table } from "react-bootstrap";
+import { UseStateContext } from "../../util/useReducerUtil/UseStateContext";
 
 export const MainPageDatabaseDisplayPanel = () => {
+
+    const { state } = UseStateContext();
 
     return(
 
         <>
+            <Table striped bordered hover>
+                <thead>
+                    <tr>
+                        <th>First Name</th>
+                        <th>Last Name</th>
+                        <th>DOB</th>
+                        <th>Status</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {state.users.map((eachUser) => {
+                        <tr>
+                            <td>
+                                {eachUser.firstname}
+                            </td>
+                            <td>
+                                {eachUser.lastname}
+                            </td>
+                            <td>
+                                {eachUser.dob}
+                            </td>
+                            <td>
+                                {eachUser.status}
+                            </td>
+                        </tr>
+                    })}
+                </tbody>
+            </Table>
+
         </>
 
     );
